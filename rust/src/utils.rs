355eb2795e84dda98817f158b72ff51bc793db41
alvarozamora <alvarozamora@stanford.edu>
2022-03-03 18:53:37 -0800

    rust: 2D FFT unit test passes

--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -1,13 +1,14 @@
-use ndarray::{array, arr1, arr2, arr3, s, Array, ShapeBuilder};
+use ndarray::{array, arr1, arr2, arr3, s, Array, Dim, Dimension, IntoDimension, ShapeBuilder, Data, Axis, ScalarOperand};
 use rustfft::{num_complex::Complex, FftPlanner, FftNum};
 use transpose::transpose;
-
+use conv::prelude::*;
 
 /// This struct is intended to be initialized at the
 /// beginning of a simulation. It holds the forward
-/// and inverse fft objects that process FFTs.
+/// and inverse fft objects that perform FFTs.
 pub struct FftObject<T, const K: usize>
-where T: FftNum
+where 
+    T: FftNum + ValueFrom<usize> + ScalarOperand,
 {
     // Planner
     pub planner: rustfft::FftPlanner<T>,
@@ -20,14 +21,22 @@ where T: FftNum
 }
 
 impl <T, const K: usize> FftObject<T, K> 
-where T: FftNum
+where
+    T: FftNum + ValueFrom<usize> + ScalarOperand,
+    Complex<T>: std::ops::DivAssign,
 {
+    /// This `FftObject` constructor takes in a `size` (which is stored)
+    /// and returns a struct containing the fft planner (`rustfft::FftPlanner<T>`)
+    /// along with the associated forward and inverse operators.
     pub fn new(size: usize) -> Self {
 
+        // Create planner of type T
         let mut planner = FftPlanner::<T>::new();
+        // Create forward and inverse plans
         let fwd = planner.plan_fft_forward(size);
         let inv = planner.plan_fft_inverse(size);
 
+        // Pack into struct and return
         FftObject {
             planner,
             fwd,
@@ -36,19 +45,121 @@ where T: FftNum
         }
     }
     
-    pub fn forward(self, data: &mut [Complex<T>]) {
+    /// This function takes
+    pub fn forward(&self, data: &mut Array<Complex<T>, Dim<[usize; K]>>) -> Result<(), MSMError>
+    where
+        T: FftNum + ValueFrom<usize> + ScalarOperand,
+        Complex<T>: std::ops::DivAssign<T>,
+        Dim<[usize; K]> : Dimension + IntoDimension,
+    
+    {
 
+        // Ensure data provided is of the size supported
+        assert_eq!(data.shape(), &[self.size; K]);
+        
+        // Forward needs division
+        //*data = *data/self.size.value_as::<T>().unwrap();
+        //data = &mut data.map(|x| x/self.size.value_as::<T>().unwrap());
+        *data /= &array![Complex{ re: self.size.pow(K as u32).value_as::<T>().unwrap(), im: T::zero()} ];
+        //*data /= array![ Complex{ re: self.size.value_as::<T>().unwrap(), im: T::zer;
 
-        assert_eq!(data.len(), self.size);
+        // Dimension dependent forward
+        match K {
+            1 => Ok(self.fwd.process(data.as_slice_mut().unwrap())),
+            2 => Ok({
+                    // Iterate through rows
+                    for mut row in data.rows_mut() {
+                        self.fwd.process(row.as_slice_mut().expect("invalid row"));
+                    }
 
-        self.fwd.process(data)
+                    // Transpose rows and columns
+                    transpose::transpose(data.clone().as_slice().unwrap(), data.as_slice_mut().unwrap(), self.size, self.size);
+            
+                    // Iterate through rows (prev columns)
+                    for mut row in data.rows_mut() { 
+                        self.fwd.process(row.as_slice_mut().expect("invalid row post-transpose"));
+                    }
+                }),
+            3 => Ok({
+                    // Iterate through axis 0
+                    for mut xlane in data.lanes_mut(Axis(0)) {
+                        self.fwd.process(xlane.as_slice_mut().expect("invalid x-axis"));
+                    }
+                    // Iterate through axis 1
+                    for mut ylane in data.lanes_mut(Axis(1)) { 
+                        self.fwd.process(ylane.as_slice_mut().expect("invalid y-axis"));
+                    }
+                    // Iterate through axis 2
+                    for mut zlane in data.lanes_mut(Axis(2)) { 
+                        self.fwd.process(zlane.as_slice_mut().expect("invalid z-axis"));
+                    }
+                }),
+            k => Err(MSMError::IncorrectNumDumensions(k))
+        }
+    }
 
+    /// This function takes
+    pub fn inverse(&self, data: &mut Array<Complex<T>, Dim<[usize; K]>>) -> Result<(), MSMError>
+    where
+        T: FftNum + ValueFrom<usize> + ScalarOperand,
+        Complex<T>: std::ops::DivAssign<T>,
+        Dim<[usize; K]> : Dimension + IntoDimension,
+    
+    {
+
+        // Ensure data provided is of the size supported
+        assert_eq!(data.shape(), &[self.size; K]);
+        
+        // Forward needs division
+        //*data = *data/self.size.value_as::<T>().unwrap();
+        //data = &mut data.map(|x| x/self.size.value_as::<T>().unwrap());
+        //*data /= &array![Complex{ re: self.size.value_as::<T>().unwrap(), im: T::zero()} ];
+        //*data /= array![ Complex{ re: self.size.value_as::<T>().unwrap(), im: T::zer;
+
+        // Dimension dependent forward
+        match K {
+            1 => Ok(self.inv.process(data.as_slice_mut().unwrap())),
+            2 => Ok({
+                    // Iterate through rows
+                    for mut row in data.rows_mut() {
+                        self.inv.process(row.as_slice_mut().expect("invalid row"));
+                    }
+
+                    // Transpose rows and columns
+                    transpose::transpose(data.clone().as_slice().unwrap(), data.as_slice_mut().unwrap(), self.size, self.size);
+            
+                    // Iterate through rows (prev columns)
+                    for mut row in data.rows_mut() { 
+                        self.inv.process(row.as_slice_mut().expect("invalid row post-transpose"));
+                    }
+                }),
+            3 => Ok({
+                    // Iterate through axis 0
+                    for mut xlane in data.lanes_mut(Axis(0)) {
+                        self.inv.process(xlane.as_slice_mut().expect("invalid x-axis"));
+                    }
+                    // Iterate through axis 1
+                    for mut ylane in data.lanes_mut(Axis(1)) { 
+                        self.inv.process(ylane.as_slice_mut().expect("invalid y-axis"));
+                    }
+                    // Iterate through axis 2
+                    for mut zlane in data.lanes_mut(Axis(2)) { 
+                        self.inv.process(zlane.as_slice_mut().expect("invalid z-axis"));
+                    }
+                }),
+            k => Err(MSMError::IncorrectNumDumensions(k))
+        }
+        
 
     }
 }
 
 //fn fourier_transform()
 
+
+pub enum MSMError {
+    IncorrectNumDumensions(usize)
+}
 
 
 #[test]
@@ -109,12 +220,11 @@ fn test_fft_object_2_d_usage() {
     let orig = data.clone();
     
     // Carry out fwd + inv FFT
-    fft.fwd.process(data.as_slice_mut().expect("invalid data"));
-    fft.inv.process(data.as_slice_mut().expect("invalid data"));
-
-    // Renormalize
-    data = data/FFT_SIZE as T;
+    fft.forward(&mut data);
+    fft.inverse(&mut data);
 
     // Check that sum of norm of elementwise difference is tiny or zero
     assert!((data-orig).map(|x| x.norm()).sum() < 1e-9);
 }
+
+
