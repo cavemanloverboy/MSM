67b22172655832623f59111a1ce370a7dddf0233
alvarozamora <alvarozamora@stanford.edu>
2022-03-04 18:42:38 -0800

    rust: cleanup and sync tests

--- rust/tests/fft.rs
+++ rust/tests/fft.rs
@@ -1,9 +1,8 @@
-use ndarray::{arr1, ShapeBuilder};
-use rustfft::{FftPlanner, num_complex::Complex};
+use ndarray::arr1;
+use rustfft::{num_complex::Complex, FftPlanner};
 
 #[test]
 fn test_fft() {
-
     // Perform a forward FFT of size FFT_SIZE
     const FFT_SIZE: usize = 16;
     type T = f32;
@@ -14,22 +13,22 @@ fn test_fft() {
     let ifft = planner.plan_fft_inverse(FFT_SIZE);
 
     // Define data, save copy for comparison
-    let mut buffer = arr1(&[Complex::<T> {re: 1.0, im: 0.0}; FFT_SIZE]);
+    let mut buffer = arr1(&[Complex::<T> { re: 1.0, im: 0.0 }; FFT_SIZE]);
     let orig = buffer.clone();
     println!("{:?}", buffer);
 
     // Do forward FFT, and divide by FFT_size
     fft.process(buffer.as_slice_mut().unwrap());
-    buffer = buffer/(FFT_SIZE as T);//.iter().map(| Complex{re:x, im:y}| Complex{re: *x/FFT_SIZE as T, im: *y/FFT_SIZE as T}).collect();
+    buffer = buffer / (FFT_SIZE as T); //.iter().map(| Complex{re:x, im:y}| Complex{re: *x/FFT_SIZE as T, im: *y/FFT_SIZE as T}).collect();
     println!("{:?}", buffer);
 
     // Do inverse FFT and assert it is close to original
     ifft.process(buffer.as_slice_mut().unwrap());
     println!("{:?}", buffer);
     assert!(
-        orig
-          .iter()
-          .zip(buffer).
-          fold(0.0,|acc, (x, y)| acc + (x.re-y.re).abs())
-        < 1.0e-6);
+        orig.iter()
+            .zip(buffer)
+            .fold(0.0, |acc, (x, y)| acc + (x.re - y.re).abs())
+            < 1.0e-6
+    );
 }
--- rust/tests/fft2D.rs
+++ rust/tests/fft2D.rs
@@ -1,11 +1,10 @@
-use ndarray::{array, arr2, s, Array, ShapeBuilder};
+use ndarray::{arr2, Array};
 use rustfft::{num_complex::Complex, FftPlanner};
 use transpose::transpose;
 
 #[test]
 fn test_2_d_fft_with_ndarray() {
     // Perform a forward FFT of size FFT_SIZE (per dimension)
-    const DIM: usize = 2;
     const FFT_SIZE: usize = 4;
     type T = f64;
 
@@ -16,9 +15,10 @@ fn test_2_d_fft_with_ndarray() {
 
     // Define data, save copy for comparison
     // This is a FFT_SIZE x FFT_SIZE array
-    let mut my_array: [[Complex::<T>; FFT_SIZE]; FFT_SIZE] = [[Complex::<T> { re: 1.0, im: -1.0 }; FFT_SIZE]; FFT_SIZE];
+    let mut my_array: [[Complex<T>; FFT_SIZE]; FFT_SIZE] =
+        [[Complex::<T> { re: 1.0, im: -1.0 }; FFT_SIZE]; FFT_SIZE];
     my_array[0][2] = Complex::<T> { re: 5.0, im: -5.0 };
-    let mut buffer: Array<Complex<T>, _> = arr2(&my_array);//array![my_array];
+    let mut buffer: Array<Complex<T>, _> = arr2(&my_array); //array![my_array];
     let orig = buffer.clone();
 
     println!("{:?}", buffer);
@@ -26,53 +26,55 @@ fn test_2_d_fft_with_ndarray() {
     // Forward FFT
     {
         for mut row in buffer.rows_mut() {
-
             println!("{}", &row);
             fft.process(row.as_slice_mut().expect("invalid row"));
         }
         println!("{}", "-".repeat(40));
-        transpose::transpose(buffer.clone().as_slice().unwrap(), buffer.as_slice_mut().unwrap(), FFT_SIZE, FFT_SIZE);
+        transpose(
+            buffer.clone().as_slice().unwrap(),
+            buffer.as_slice_mut().unwrap(),
+            FFT_SIZE,
+            FFT_SIZE,
+        );
 
-        for mut row in buffer.rows_mut() { 
-
+        for mut row in buffer.rows_mut() {
             println!("{}", &row);
             fft.process(row.as_slice_mut().expect("invalid row post-transpose"));
         }
     }
     println!("{}", "-".repeat(40));
     // Inverse FFT
-    //transpose::transpose(buffer.clone().as_slice().unwrap(), buffer.as_slice_mut().unwrap(), FFT_SIZE, FFT_SIZE);
     {
         for mut row in buffer.rows_mut() {
-
             println!("{}", &row);
             ifft.process(row.as_slice_mut().expect("invalid row"));
         }
 
         println!("{}", "-".repeat(40));
-        transpose::transpose(buffer.clone().as_slice().unwrap(), buffer.as_slice_mut().unwrap(), FFT_SIZE, FFT_SIZE);
+        transpose(
+            buffer.clone().as_slice().unwrap(),
+            buffer.as_slice_mut().unwrap(),
+            FFT_SIZE,
+            FFT_SIZE,
+        );
 
-        for mut row in buffer.rows_mut() { 
-
+        for mut row in buffer.rows_mut() {
             println!("{}", &row);
             //println!("{:?}", row.as_slice_mut());
             ifft.process(row.as_slice_mut().expect("invalid row post-transpose"));
         }
     }
 
-   
     println!("{}", "-".repeat(40));
-    for row in buffer.rows() { 
-
+    for row in buffer.rows() {
         println!("{}", &row);
     }
-    buffer = buffer/(FFT_SIZE as T)/(FFT_SIZE as T); //* arr2(&[[Complex{ re: 1.0/(FFT_SIZE as T)/(FFT_SIZE as T), im: 1.0/(FFT_SIZE as T)/(FFT_SIZE as T) }; FFT_SIZE]; FFT_SIZE]);//::Array<Complex<T>,_>();
+    buffer = buffer / (FFT_SIZE as T) / (FFT_SIZE as T); //* arr2(&[[Complex{ re: 1.0/(FFT_SIZE as T)/(FFT_SIZE as T), im: 1.0/(FFT_SIZE as T)/(FFT_SIZE as T) }; FFT_SIZE]; FFT_SIZE]);//::Array<Complex<T>,_>();
     println!("{}", "-".repeat(40));
-    for row in buffer.rows() { 
-
+    for row in buffer.rows() {
         println!("{}", &row);
     }
     println!("{:?}", buffer.map(|x| x.norm()));
 
-    assert!((buffer-orig).map(|x| x.norm()).sum() < 1e-6)
+    assert!((buffer - orig).map(|x| x.norm()).sum() < 1e-6)
 }
