af3bccd2e185e231ee51c46baecf0a1752bcbc55
Alvaro Zamora <alvarozamora@stanford.edu>
2022-04-12 14:50:37 -0700

    plot labels

--- rust/plot.py
+++ rust/plot.py
@@ -20,15 +20,25 @@ with imageio.get_writer('coldgauss.mp4', fps=24) as writer:
 
 		plt.figure(1, figsize=(16,8))
 		plt.subplot(121)
+		plt.xlabel("x (grid cell)")
+		plt.ylabel("y (grid cell)")
+		plt.title("Projected Spatial Density")
 
 		# this somewhat centers seed=0, N=128
 		#plt.imshow(np.roll(np.roll(psi_sq.mean(0), -32, 1), -64,0))
 
 		# this somehwat centers seed=0, N=256
 		plt.imshow(np.roll(np.roll(psi_sq.mean(0), 0, 1), -128,0))
-		
+
+		# this somehwat centers seed=0, N=512, f64
+		plt.imshow(np.roll(np.roll(psi_sq.mean(0), -328, 1), -100,0))
+
 		plt.subplot(122)
 		plt.imshow(psi_k_sq.mean(0))
+		plt.xlabel("kx (grid cell)")
+		plt.ylabel("ky (grid cell)")
+		plt.title("Projected Momentum Density")
+
 		buf = io.BytesIO()
 		plt.savefig(buf)
 		buf.seek(0)
