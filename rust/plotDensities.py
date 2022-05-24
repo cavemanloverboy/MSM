e6db2c68926067ae2b5aac4c6833d3194fcb35b1
andillio <aeberhar@stanford.edu>
2022-05-23 17:22:51 -0700

    finished plot densities

--- rust/plotDensities.py
+++ rust/plotDensities.py
@@ -53,16 +53,16 @@ def radial_profile(drop):
 	for r in R:
 		M_r.append(rho[diff < r].sum())
 	
-	rho = [M_r[0] / (4.*np.pi) / (dx/2.)**2 / dx]
+	rho_r = [M_r[0] / (4.*np.pi) / (dx/2.)**2 / dx]
 	for i in range(1,len(M_r)):
 		r_diff = (R[i] + R[i-1])/2.
 		M_diff = M_r[1] - M[i-1]
-		rho.append(M_diff / (4.*np.pi) / (r_diff)**2 / dx)
+		rho_r.append(M_diff / (4.*np.pi) / (r_diff)**2 / dx)
 	
 	plt.figure(figsize=(24,8))
 	plt.subplot(131)
-	plt.xlabel(r"$x, \, [kpc]$")
-	plt.ylabel(r"$y, \, [kpc]$")
+	plt.xlabel(r"$x \, [kpc]$")
+	plt.ylabel(r"$y \, [kpc]$")
 	plt.title("Projected Spatial Density")
 
 	plt.imshow(rho.mean(0), interpolation = 'none', 
@@ -73,19 +73,70 @@ def radial_profile(drop):
 	plt.imshow(psi_k_sq.mean(0), interpolation = 'none', 
         extent=[np.min(u),np.max(u), np.min(u),np.max(u)], 
         aspect = 'auto', origin = 'lower',cmap = 'viridis')
-	plt.xlabel(r"$u_x, \, [kpc / Myr]$")
-	plt.ylabel(r"$u_y, \, [kpc / Myr]$")
+	plt.xlabel(r"$u_x \, [kpc / Myr]$")
+	plt.ylabel(r"$u_y \, [kpc / Myr]$")
 	plt.title("Projected Momentum Density")
 
+	plt.subplot(133)
+	plt.cla()
+	plt.loglog(R, rho_r)
+	plt.xlabel(r"$r \, [kpc]$")
+	plt.ylabel(r'$\rho(r)$')
+	plt.title("Enclosed mass")
+
+	buf = io.BytesIO()
+	plt.savefig(buf)
+	buf.seek(0)
+
+	return buf
 
 def end_and_final_radial_profile():
-
+    	
 	drop0 = drops[0]
 	psi = np.load(drop0)
-	psi_k = np.fft.fftshift(np.fft.fftn(psi['real'] + 1j*psi['imag']))
-	psi_k_sq = np.abs(psi_k)**2
+	rho = psi['real']**2 + psi['imag']**2)
+	rho /= rho.sum()
+	rho *= Mtot 
 
-	rho = psi['real']**2 + psi['imag']**2
+	R = np.linspace(dx, L/4, 32)
+
+	grid = np.arange(rho.shape[0])*dx
+	xx, yy, zz = np.meshgrid(grid, grid, grid)
+	center = L/2
+	diff = np.sqrt( (xx-center)**2 + (yy-center)**2 + (zz-center)**2)
+
+	M_r = []
+	for r in R:
+		M_r.append(rho[diff < r].sum())
+
+	plt.figure(figsize=(12,8))
+	plt.loglog(R, M_r, label="initial")
+
+	drop_final = drops[-1]
+	psi = np.load(drop_final)
+	rho = psi['real']**2 + psi['imag']**2)
+	rho /= rho.sum()
+	rho *= Mtot 
+
+	R = np.linspace(dx, L/4, 32)
+
+	grid = np.arange(rho.shape[0])*dx
+	xx, yy, zz = np.meshgrid(grid, grid, grid)
+	center = L/2
+	diff = np.sqrt( (xx-center)**2 + (yy-center)**2 + (zz-center)**2)
+
+	M_r = []
+	for r in R:
+		M_r.append(rho[diff < r].sum())
+
+	plt.loglog(R, M_r, label="final")
+
+	plt.xlabel("pixels")
+	plt.ylabel("mass enclosed within pixels")
+	plt.title("Enclosed mass")
+	plt.legend()
+
+	plt.savefig("profile.png", dpi=200)
 
 if __name__ == "__main__":
 	start = time()
