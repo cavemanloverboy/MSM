correct average

msm-data-interpreter/di.py
@@ -1,4 +1,3 @@
-from stat import S_ENFMT
 import numpy as np
 from glob import glob
 import os
@@ -114,25 +113,25 @@ def torch_analyze_sims(
     while len(size) < 4:
         size = np.append(size, 1)
 
-    # Define 
-    psi = torch.zeros(tuple(s for s in size)).to(device) * (0 + 0j)
-    psi2 = torch.zeros(tuple(s for s in size)).to(device)
-    psik = torch.zeros(tuple(s for s in size)).to(device) * (0 + 0j)
-    psik2 = torch.zeros(tuple(s for s in size)).to(device)
-
 
     # Iterate through each dump
     it = tqdm(ndumps)
     for dump in it:
-
-        # Define dataset containing same dump from every dataset
-        real_dump_names = np.sort(glob(f"{sim_data_dir}/{sim_name}-stream*/psi_{dump:05d}_real"))
 
         # Create combined directory if it does not yet exist
         combined_path = f"{sim_data_dir}/{sim_name}-combined"
         os.makedirs(combined_path, exist_ok=True)
         if os.path.exists(f"{combined_path}/psi_{dump:05d}"):
             continue
+
+        # Define buffers for reduce operations
+        psi = torch.zeros(tuple(s for s in size)).to(device) * (0 + 0j)
+        psi2 = torch.zeros(tuple(s for s in size)).to(device)
+        psik = torch.zeros(tuple(s for s in size)).to(device) * (0 + 0j)
+        psik2 = torch.zeros(tuple(s for s in size)).to(device)
+
+        # Define dataset containing same dump from every dataset
+        real_dump_names = np.sort(glob(f"{sim_data_dir}/{sim_name}-stream*/psi_{dump:05d}_real"))
 
         # Create dataloader
         dataset = DumpDataset(real_dump_names)
@@ -173,7 +172,7 @@ if __name__ == "__main__":
 
     sim_name = "gaussian-overdensity-512"
     sim_data_dir = "../rust/sim_data"
-    dumps = np.arange(19,100+1)
+    dumps = np.arange(0,101)
     K = 3
     S = 512
 